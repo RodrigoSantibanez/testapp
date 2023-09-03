2b5021d417e93f4f4dda070a530f5c7cd1ceabaa
RodrigoSantibanez <rosantibanezs@gmail.com>
2023-09-03 18:05:25 -0300

    changes03/09

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-login',
@@ -12,20 +14,43 @@ export class LoginPage implements OnInit {
     password: ''
   };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private alertController: AlertController) {}
 
   ngOnInit() {}
 
-  ingresar() {
-    const navigationExtras: NavigationExtras = {
-      state: {
-        user: this.user
-      }
-    };
-    this.router.navigate(['/home'], navigationExtras);
-  }
+  //ingresar() {
+  //  const navigationExtras: NavigationExtras = {
+  //    state: {
+  //      user: this.user
+  //    }
+  //  };
+  //  this.router.navigate(['/home'], navigationExtras);
+  //}
 
   routerRestablecer(){
     this.router.navigate(['/restablecer']);
   }
+
+  async ingresar() {
+    const minLength = 4; // Cambia esto al número mínimo de caracteres
+
+    if (this.user.usuario.length < minLength) {
+      const alert = await this.alertController.create({
+        header: 'Datos invalidos',
+        message: `El nombre de usuario debe tener al menos ${minLength} caracteres.`,
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    } else {
+      const navigationExtras: NavigationExtras = {
+        state: {
+          user: this.user,
+        },
+      };
+      this.router.navigate(['/home'], navigationExtras);
+    }
+  }
+  
 }
